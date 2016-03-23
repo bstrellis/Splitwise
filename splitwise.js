0b961f776781e65321149ea9ce8c89fbe07b2873
Brandon Strellis <bstrellis@gmail.com>
2016-03-23 14:59:16 -0700

    courtland corrected the signup page input fields so that it stores a string to localstorage

--- splitwise.js
+++ splitwise.js
@@ -1,38 +1,97 @@
-// TODO THIS COMMIT
-// add basic html to homepage
-// TODO Next commit
+// TODO This commit
 // add basic functionality:
 // name autopopulates based on user
 // groups autopopulates
 // friends autopopulates
+// all data is stored in local storage
 
-// run when page first loads - not running this now bc it's a pain
-// setSignUpPage();
+var templateStrings = {
+  signUpPage: '\
+    <div class="screen">\
+      <div>My name is...</div>\
+      <input class="user-name" type="text">\
+      <div>My email is...</div>\
+      <input class="user-email" type="text">\
+      <div>My password is...</div>\
+      <input class="user-password" type="text">\
+      <button class="sign-up-button">sign up</button>\
+    </div>',
+
+  homePage: {
+    dashboard: '\
+      <div class="console-shelf">\
+        <div>dashboard</div>\
+        <button>add a bill</button>\
+        <button>settle up</button>\
+      </div>\
+      <div class="console-shelf">\
+        <div>\
+          <div>total balance</div>\
+          <div>$90</div>\
+        </div>\
+        <div>\
+          <div>you owe</div>\
+          <div>$90</div>\
+        </div>\
+        <div>\
+          <div>you are owed</div>\
+          <div>$90</div>\
+        </div>\
+      </div>\
+      <div class="console-shelf">\
+        <div>YOU OWE</div>\
+        <div>listchart</div>\
+        <div>YOU ARE OWED</div>\
+      </div>\
+      <div class="console-shelf">\
+        <div>YOU OWE item</div>\
+        <div>YOU ARE OWED item</div>\
+      </div>',
+    main: '\
+      <div class="screen">\
+        <div class="header">\
+          <div>splitwise</div>\
+          <div class="session-owner">logout from {{user-name}}</div>\
+        </div>\
+        <div class="homepage">\
+          <div class="navigation">\
+            <div>dashboard</div>\
+            <div>recent activity</div>\
+            <div>all expenses</div>\
+            <div>groups</div>\
+            <div>friends</div>\
+          </div>\
+          <div class="console">\
+            {{console-content}}\
+          </div>\
+          <div class="ad-space"></div>\
+        </div>\
+      </div>'
+  }
+};
+
+// removes all html nested within body element
+function clearScreen() {
+  document.body.removeChild(document.querySelector('.screen'));
+}
 
 // sets up Sign Up Page for new users
-// TODO at some point: bypass sign up page for logged in user
 function setSignUpPage() {
-  var htmlElements = document.body.innerHTML = '\
-    <div class = "screen"><div>My name is...</div>\
-      <input class = "userName" type="text">\
-      <div>My email is...</div>\
-      <input class = "userEmail" type="text">\
-      <div>My password is...</div>\
-      <input class = "userPassword" type="text">\
-      <button class = "sign-up-button">sign up</button>\
-    </div>';
+  var htmlElements = document.body.innerHTML = templateStrings.signUpPage;
 
   document.body.querySelector('.sign-up-button').addEventListener('click', storeSignUpData);
 }
 
 // Stores name, email, and pw of new user in local storage, then calls
 // setHomepage to move user to dashboard
-function storeSignUpData(){
-  var userName = document.getElementsByClassName('userName');
+function storeSignUpData() {
+  var userName = document.querySelector('.user-name').value;
   localStorage.setItem('session.user', userName);
-  var userEmail = document.getElementById('userEmail');
+
+  var userEmail = document.querySelector('.user-email').value;
   localStorage.setItem('session.email', userEmail);
-  var userPassword = document.getElementById('userPassword');
+
+  var userPassword = document.querySelector('.user-password').value;
   localStorage.setItem('session.password', userPassword);
 
   setHomepage();
@@ -42,66 +101,15 @@ function storeSignUpData(){
 function setHomepage() {
   // clearScreen();
 
-  var homepageElements = document.body.innerHTML = '\
-    <div class="screen">\
-      <div class="header">\
-        <div>splitwise</div>\
-        <div>session owner</div>\
-      </div>\
-      <div class="homepage">\
-        <div class="navigation">\
-          <div>dashboard</div>\
-          <div>recent activity</div>\
-          <div>all expenses</div>\
-          <div>groups</div>\
-          <div>friends</div>\
-        </div>\
-        <div class="console">\
-          <div class = "console-shelf">\
-            <div>dashboard</div>\
-            <button>add a bill</button>\
-            <button>settle up</button>\
-          </div>\
-          <div class = "console-shelf">\
-            <div>\
-              <div>total balance</div>\
-              <div>$90</div>\
-            </div>\
-            <div>\
-              <div>you owe</div>\
-              <div>$90</div>\
-            </div>\
-            <div>\
-              <div>you are owed</div>\
-              <div>$90</div>\
-            </div>\
-          </div>\
-          <div class = "console-shelf">\
-            <div>YOU OWE</div>\
-            <div>listchart</div>\
-            <div>YOU ARE OWED</div>\
-          </div>\
-          <div class = "console-shelf">\
-            <div>YOU OWE item</div>\
-            <div>YOU ARE OWED item</div>\
-          </div>\
-        </div>\
-        <div class="ad-space"></div>\
-      </div>\
-    </div>';
+  var htmlStr = templateStrings.homePage.main;
+  htmlStr = htmlStr.replace('{{console-content}}', templateStrings.homePage.dashboard);
+
+  var userName = localStorage.getItem('session.user');
+  htmlStr = htmlStr.replace('{{user-name}}', userName);
+
+  var homepageElements = document.body.innerHTML = htmlStr;
 }
 
-setHomepage();
+setSignUpPage();
 
-// removes all html nested within body element
-function clearScreen() {
-  document.body.removeChild(document.querySelector('.screen'));
-}
-
-
-
-
-// function get(){
-//     var one = document.getElementById('1').value
-//     one = localStorage.getItem("kidStore");
-// }
+// setHomepage();
