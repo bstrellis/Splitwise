90d1848095790b26ae8a6c2baa40ce4379716b04
Brandon Strellis <bstrellis@gmail.com>
2016-03-24 10:20:34 -0700

    added function to populate local storage with user/bill/payment/group

--- splitwise.js
+++ splitwise.js
@@ -1,4 +1,50 @@
+// TODO THIS COMMIT
+function to populate groups, bills, friends, any other needed data
+// TODO THIS COMMIT
+// find userID from email address, if it doesn't exist, go to sign up, if it does
+// go to homepage
 
+function populateLocalStorage() {
+  localStorage.setItem('group.0.name', '1461 Pine St');
+  localStorage.setItem('group.1.name', 'Cali House Trip');
+  localStorage.setItem('group.2.name', 'Yosemite Camping Trip');
+
+  localStorage.setItem('bill.0.amount', '140.00');
+  localStorage.setItem('bill.0.date', 'January 13, 2016');
+  localStorage.setItem('bill.0.name', 'cheetos');
+  localStorage.setItem('bill.0.groupId', '0');
+  localStorage.setItem('bill.1.amount', '7.00');
+  localStorage.setItem('bill.1.date', 'February 3, 2016');
+  localStorage.setItem('bill.1.name', 'corn pops');
+  localStorage.setItem('bill.1.groupId', '1');
+  localStorage.setItem('bill.2.amount', '80.00');
+  localStorage.setItem('bill.2.date', 'March 22, 2016');
+  localStorage.setItem('bill.2.name', 'condoms');
+  localStorage.setItem('bill.2.groupId', '2');
+
+  localStorage.setItem('user.0.email', 'bob@bob.com');
+  localStorage.setItem('user.0.name', 'bob');
+  localStorage.setItem('user.0.groupIds', '0,1');
+  localStorage.setItem('user.1.email', 'joe@joe.com');
+  localStorage.setItem('user.1.name', 'joe');
+  localStorage.setItem('user.1.groupIds', '0,1');
+  localStorage.setItem('user.2.email', 'terrance@terrance.com');
+  localStorage.setItem('user.2.name', 'terrance');
+  localStorage.setItem('user.2.groupIds', '0,2');
+
+  localStorage.setItem('payment.0.amount', '130.00');
+  localStorage.setItem('payment.0.date', 'January 16, 2016');
+  localStorage.setItem('payment.0.billId', '0');
+  localStorage.setItem('payment.0.userId', '1');
+  localStorage.setItem('payment.1.amount', '30.00');
+  localStorage.setItem('payment.1.date', 'February 28, 2016');
+  localStorage.setItem('payment.1.billId', '1');
+  localStorage.setItem('payment.1.userId', '0');
+  localStorage.setItem('payment.2.amount', '100.00');
+  localStorage.setItem('payment.2.date', 'March 27, 2016');
+  localStorage.setItem('payment.2.billId', '2');
+  localStorage.setItem('payment.2.userId', '2');
+}
 
 var templateStrings = {
   signUpPage: '\
@@ -127,3 +173,6 @@ if (isKnownUser === 'true') {
 } else {
   drawSignUpPage();
 }
+
+// populate local storage when loading page
+populateLocalStorage();
